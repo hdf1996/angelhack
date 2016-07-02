734f217046d42a0efc5343d91efc646b76bed7c3
Hugo Farji <hugo@6700.com.ar>
2016-07-02 15:26:39 -0300

    nadie mire estos commits

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
   	resources :events
+  	resources :charts, only:[:index, :show]
   end
+
 
 end
