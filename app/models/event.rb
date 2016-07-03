8ff168921a8c79f4e8bbee9f176cd5db6ff7619a
Ariel Gerstein <ariel.ger@hotmail.com>
2016-07-03 12:54:32 -0300

    WIP

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ActiveRecord::Base
   validates :name, :date, :value, presence: true
 
-  scope :created_between, ->(from, to) {where('date > ? AND date < ?', from, to)}
   scope :by_user_id, -> (user_id) {where(user_id: user_id)}
   scope :relative_to, -> date { where date: (date-24.hours)..(date+24.hours) }
 end
