3bea391edad297c65b093f34848470139acdd7e4
Pablo Francisco Mescher <pablo.mescher@altoros.com>
2016-07-02 21:06:51 -0300

    Charts wip

--- public/app/directives/charts.js
+++ public/app/directives/charts.js
@@ -5,38 +5,25 @@ angular.module("logu")
       restrict: "AE",
       templateUrl: "app/partials/charts.html",
       scope: {
-        sets: "="
+        sets: "=",
+        from: "=",
+        to: "=",
+        name: "="
       },
       link: function (scope, elem, attr) {
         "use strict";
         var canvas = elem.find("canvas").eq(0);
-        var options = {
-          type: 'line',
-          data: {
-            // Etiquetas en el eje X en este caso los dias
-            labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
-          },
-          options: {
-            // En true toma el ancho y alto de la caja padre
-            responsive: false,
-            // Titulo del Chart
-            title: {
-              display: true,
-              text: "Demo LogÜ"
-            },
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
-            }
-          }
-        };
+
+        if (!scope.from) {
+          scope.from = "";
+        }
+
+        if (!scope.to) {
+          scope.to = "";
+        }
 
 
-        /*
-        $http.get("/api/charts", {
+        $http.get("/api/charts?from=" + scope.from + "&to=" + scope.to + "&name=" + scope.name, {
           headers: {
             user_id: localStorage.getItem("user")
           }
@@ -50,8 +37,9 @@ angular.module("logu")
               label: dataset.label,
               data: dataset.data,
               fill: false,
+              snapGaps: true,
               borderWidth: 1,
-              backgroundColor: 'rgba(' + red + ',' + green + ',' + blue + ',0.2)',
+              backgroundColor: 'rgba(' + red + ',' + green + ',' + blue + ', 0.2)',
               borderColor: 'rgba(' + red + ',' + green + ',' + blue + ',1)'
             }
           });
@@ -59,15 +47,13 @@ angular.module("logu")
         }, function (response) {
           console.error(response.data);
         });
-        */
 
+        /*
         options.data.datasets = [
           {
             // Titulo que representa este set de datos
             label: 'Horas de Sueño',
-            /* dataJson[0].name */
             data: [5, 4, 5, 6, 8, 7, 3],
-            /* dataJson[0].data */
             fill: false,
             backgroundColor: "rgba(255,99,132,0.2)",
             borderColor: "rgba(255,99,132,1)",
@@ -76,15 +62,14 @@ angular.module("logu")
           {
             // Titulo que representa este set de datos
             label: 'Horas de Actividad Fisica',
-            /* dataJson[1].name */
             data: [2, 3, 2, 1, 1, 4, 2],
-            /* dataJson[1].data */
             fill: false,
             backgroundColor: "rgba(255,99,132,0.2)",
             borderColor: "rgba(108, 77, 93,1)",
             borderWidth: 1
           }
         ];
+      */
 
         new Chart(canvas, options);
       }
